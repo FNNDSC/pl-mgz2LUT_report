c44521cd0a60740e3f32b9a851a9d8fa15a731ab
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2020-09-15 13:18:21 -0400

    Added parameters and working code to the repo

--- mgz2lut_report/mgz2lut_report.py
+++ mgz2lut_report/mgz2lut_report.py
@@ -12,6 +12,11 @@
 
 import os
 import sys
+import nibabel as nib
+import numpy as np
+import collections
+import re
+import pandas as pd
 sys.path.append(os.path.dirname(__file__))
 
 # import the Chris app superclass
@@ -19,9 +24,14 @@ from chrisapp.base import ChrisApp
 
 
 Gstr_title = """
-
-Generate a title from 
-http://patorjk.com/software/taag/#p=display&f=Doom&t=mgz2lut_report
+                      _____  _       _                               _   
+                     / __  \| |     | |                             | |  
+ _ __ ___   __ _ ____`' / /'| |_   _| |_   _ __ ___ _ __   ___  _ __| |_ 
+| '_ ` _ \ / _` |_  /  / /  | | | | | __| | '__/ _ \ '_ \ / _ \| '__| __|
+| | | | | | (_| |/ / ./ /___| | |_| | |_  | | |  __/ |_) | (_) | |  | |_ 
+|_| |_| |_|\__, /___|\_____/|_|\__,_|\__| |_|  \___| .__/ \___/|_|   \__|
+            __/ |                     ______       | |                   
+           |___/                     |______|      |_|                   
 
 """
 
@@ -96,7 +106,7 @@ class Mgz2lut_report(ChrisApp):
     SELFPATH                = os.path.dirname(os.path.abspath(__file__))
     SELFEXEC                = os.path.basename(__file__)
     EXECSHELL               = 'python3'
-    TITLE                   = 'A segmented 'mgz' reporting app'
+    TITLE                   = 'A segmented mgz reporting app'
     CATEGORY                = ''
     TYPE                    = 'ds'
     DESCRIPTION             = 'An app to generate a report on volumes of various brain segments listed in a Look-up Table (Default = FreeSurferLUT.txt)'
@@ -131,14 +141,61 @@ class Mgz2lut_report(ChrisApp):
         Define the CLI arguments accepted by this plugin app.
         Use self.add_argument to specify a new app argument.
         """
-
+        self.add_argument('--file_name', dest='file_name',type = str,optional = True, help="Segmented mgz file name", default="aparc.a2009s+aseg.mgz")
+        self.add_argument('--report_name', dest='report_name',type = str,optional = True, help="Output report name", default="mgz2LUT_report")
+        self.add_argument('--report_types', dest='report_types',type = str,optional = True, help="comma separated output report file types", default="pdf")
+        self.add_argument('--LUT', dest='LUT',type = str,optional = True, help="Look Up File Name", default="FreeSurferColorLUT.txt")
     def run(self, options):
         """
         Define the code to be run by this plugin app.
         """
         print(Gstr_title)
         print('Version: %s' % self.get_version())
+        file_path = os.path.join(options.inputdir,options.file_name)
+        # Load the file
+        print("Loading file from %s" %file_path)
+        mgz_file = nib.load(file_path)
 
+        npy_file = mgz_file.get_fdata()
+        npy_file = npy_file.astype(np.uint32)
+
+        data_list=npy_file.flatten()
+        counter=collections.Counter(data_list)
+
+        # Load Look Up file
+        print("Loading look up file from %s" %options.LUT)
+        l_column_names = ["#No", "LabelName","R","G","B"]
+        df_FSColorLUT = pd.DataFrame(columns=l_column_names)
+        with open(options.LUT) as f:
+            for line in f:
+                if line and line[0].isdigit():
+                    line = re.sub(' +', ' ', line)
+                    l_line = line.split(' ')
+                    l_labels = l_line[:5]
+                    df_FSColorLUT.loc[len(df_FSColorLUT)] = l_labels
+                    
+        # split the report types
+        report_types = options.report_types.split(',')
+        for report_type in report_types:
+            
+            report_path = ("%s/%s.%s" %(options.outputdir,options.report_name,report_type))
+            # Write and Save report
+            print("Writing report as %s" %report_path)
+            f = open(report_path,'a')
+            f.truncate(0)
+            f.write("Index \t Label Name \t Volume \n")
+            line_count = 1
+            for k in sorted(counter.keys()):
+                res_df=df_FSColorLUT.loc[df_FSColorLUT['#No'] == str(k),['LabelName']]
+
+                f.write ("%s\t%s\t%s%s" %(line_count, res_df['LabelName'].to_string(index=False),counter[k] ,"cc"))
+                f.write('\n')
+                line_count = line_count + 1
+            f.close()
+        
+        f = open(report_path,'r')
+        print(f.read())
+        print("Report saved as %s in %s format(s)" %(options.report_name, options.report_types))
     def show_man_page(self):
         """
         Print the app's man page.
